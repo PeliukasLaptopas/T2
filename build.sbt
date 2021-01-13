9abe642e85e9ed79690d550b48ef4f87ef5a6f3b
Rūtenis Naugžemis <rutenis.naugzemis@telesoftas.com>
2021-01-13 10:14:46 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Spark2"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.1"
+libraryDependencies += "com.github.wookietreiber" %% "scala-chart" % "latest.integration"
+libraryDependencies += "org.scalafx" % "scalafx_2.11" % "8.0.102-R11",
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,62 @@
+import javafx.scene.chart.ScatterChart
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.types.{CalendarIntervalType, DateType, DoubleType, FloatType, IntegerType, LongType, StringType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.functions.desc
+import org.apache.spark.sql.functions.asc
+import org.apache.spark.sql.functions.expr
+import org.apache.spark.sql.functions.{col, concat, hour, mean, lit, second, stddev, to_date, unix_timestamp}
+import org.jfree.chart.plot.Plot
+import scalafx.application.JFXApp
+import scalafx.scene.chart
+import scalafx.scene.chart.ScatterChart
+
+object Hello {
+  def main(args: Array[String]) = {
+    val spark = org.apache.spark.sql.SparkSession.builder
+      .master("local") //# Change it as per your cluster
+      .appName("Spark CSV Reader")
+      .getOrCreate;
+
+    val sc = new StructType()
+      .add(StructField("VendorID", IntegerType, false))
+      .add(StructField("tpep_pickup_datetime", TimestampType, false))
+      .add(StructField("tpep_dropoff_datetime", TimestampType, false))
+      .add(StructField("passenger_count", IntegerType, false))
+      .add(StructField("trip_distance", FloatType, false))
+      .add(StructField("RatecodeID", IntegerType, false))
+      .add(StructField("store_and_fwd_flag", StringType, false))
+      .add(StructField("PULocationID", StringType, false))
+      .add(StructField("DOLocationID", StringType, false))
+      .add(StructField("payment_type", StringType, false))
+      .add(StructField("fare_amount", StringType, false))
+      .add(StructField("extra", StringType, false))
+      .add(StructField("mta_tax", StringType, false))
+      .add(StructField("tip_amount", StringType, false))
+      .add(StructField("tolls_amount", StringType, false))
+      .add(StructField("improvement_surcharge", StringType, false))
+      .add(StructField("total_amount", StringType, false))
+      .add(StructField("congestion_surcharge", StringType, false))
+
+
+    val taxiDataFrame = spark.read.option("header", "true").schema(sc).csv("yellow_tripdata_2019-09.csv" /*"smaller.csv"*/)
+    val tripDistanceDf = taxiDataFrame.select("trip_distance", "PULocationId", "DOLocationId", "total_amount")
+    val tripDistanceStatsDf =
+      tripDistanceDf
+      .agg(
+        mean("trip_distance").as("mean"),
+        stddev("trip_distance").as("stddev")
+      )
+      .withColumn("UpperLimit", col("mean") + col("stddev") * 6)
+      .withColumn("LowerLimit", col("mean") - col("stddev") * 6)
+
+    val detectOutlier = (values: Column, upperLimit: Column, lowerLimit: Column) =>
+      (values < lowerLimit) or (values > upperLimit)
+
+    tripDistanceDf.join(tripDistanceStatsDf)
+      .withColumn("isOutlier", detectOutlier(col("trip_distance"), col("UpperLimit"), col("LowerLimit"))).
+      filter(col("isOutlier")).orderBy("total_amount").take(10).foreach(println)
+
+
+    spark.stop()
+  }
+}
